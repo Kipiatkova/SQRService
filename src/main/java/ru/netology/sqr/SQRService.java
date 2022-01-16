58419180901e883c14c2024f1819b42dd5beb263
Kipiatkova <kipiatkova1998@mail.ru>
2022-01-16 16:45:30 +0300

    JUnit tets

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,11 +1,19 @@
 package ru.netology.sqr;
 
-public class SQRService {
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-    public static void main (String[] args) {
+public class SQRService {
+    private Object SQRService;
+
+    @ParameterizedTest
+ @CsvSource(value = {"300, 800",
+ "1, 99"})
+     void shouldCalculateSQR (int a, int b) {
+     SQRService = new SQRService();
         for (int i = 10; i <= 99; i++) {
-            if (i * i > 300) {
-                if (i * i < 800)
+            if (i * i > a) {
+                if (i * i < b)
                 System.out.println("Проход в цикле " + i);
             }
         }
